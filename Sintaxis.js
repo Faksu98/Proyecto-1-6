390a9bba8c15f243c3496af9d5f3af58a8ef6629
faksu <dk>
2022-04-19 11:35:43 -0300

    ventana

---
+++ Sintaxis.js
@@ -0,0 +1,14 @@
+var respuesta = prompt("Titulo", ["Nombre"]);
+
+console.log( respuesta )
+
+document.write(`
+  <h4>Nombre del usuario</4>
+  <h4> ${ respuesta } </4>
+
+`)
+
+var string = "texto";
+var numero = 2;
+var decimal = 3.4;
+var booleano = true;
